0c32e8e87b9630e78c5c0d8bf8f8897f1e3d87f0
Marian Zharchynskyi <marianzharchynskyi@gmail.com>
2025-10-02 00:28:10 +0300

    chore: added new tests, fixed them on pipeline

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,32 +1,50 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:movie_discovery_app/main.dart';
+import 'package:movie_discovery_app/features/movies/domain/usecases/get_popular_movies.dart';
+import 'package:movie_discovery_app/features/movies/presentation/providers/movie_provider.dart';
 import 'package:movie_discovery_app/features/movies/presentation/screens/home_screen.dart';
 
-void main() {
-  testWidgets('App should show splash screen on startup', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const ProviderScope(
-        child: MaterialApp(
-          home: MovieDiscoveryApp(),
-        ),
-      ),
-    );
+// Mock GetPopularMovies
+class MockGetPopularMovies extends Mock implements GetPopularMovies {}
 
-    expect(find.byType(HomeScreen), findsOneWidget);
+void main() {
+  late MockGetPopularMovies mockGetPopularMovies;
+
+  setUp(() {
+    mockGetPopularMovies = MockGetPopularMovies();
   });
 
-  testWidgets('App should have correct title', (WidgetTester tester) async {
+  testWidgets('App should show HomeScreen on startup', (WidgetTester tester) async {
+    // Setup mock response
+    when(() => mockGetPopularMovies())
+        .thenAnswer((_) async => const Right([]));
+
+    // Build our app with the real provider but with mocked dependencies
     await tester.pumpWidget(
-      const ProviderScope(
-        child: MaterialApp(
+      ProviderScope(
+        overrides: [
+          getPopularMoviesProvider.overrideWithValue(mockGetPopularMovies),
+        ],
+        child: const MaterialApp(
           home: MovieDiscoveryApp(),
         ),
       ),
     );
-
-    final titleFinder = find.text('Movie Discovery');
-    expect(titleFinder, findsOneWidget);
+    
+    // Wait for the first frame
+    await tester.pump();
+    
+    // Wait for any remaining animations
+    await tester.pumpAndSettle();
+    
+    // Verify HomeScreen is displayed
+    expect(find.byType(HomeScreen), findsOneWidget);
+    
+    // Verify the AppBar title is displayed
+    expect(find.text('Popular Movies'), findsOneWidget);
   });
 }
