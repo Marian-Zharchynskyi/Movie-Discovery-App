a74a40fe7ea1bfae479c1798349a7abc9dc9e3b1
Marian Zharchynskyi <marianzharchynskyi@gmail.com>
2025-10-08 21:50:14 +0300

    additional to previous comit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,15 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:movie_discovery_app/main.dart';
+import 'package:movie_discovery_app/core/router/app_router.dart';
 import 'package:movie_discovery_app/features/movies/domain/usecases/get_popular_movies.dart';
 import 'package:movie_discovery_app/features/movies/presentation/providers/movie_provider.dart';
 import 'package:movie_discovery_app/features/movies/presentation/screens/home_screen.dart';
+import 'package:movie_discovery_app/features/settings/presentation/providers/settings_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:movie_discovery_app/main.dart';
 
 // Mock GetPopularMovies
 class MockGetPopularMovies extends Mock implements GetPopularMovies {}
@@ -23,15 +26,28 @@ void main() {
     when(() => mockGetPopularMovies())
         .thenAnswer((_) async => const Right([]));
 
-    // Build our app with the real provider but with mocked dependencies
+    // Build our app with provider overrides
+    SharedPreferences.setMockInitialValues({});
+    final prefs = await SharedPreferences.getInstance();
     await tester.pumpWidget(
       ProviderScope(
         overrides: [
           getPopularMoviesProvider.overrideWithValue(mockGetPopularMovies),
+          // Provide in-memory SharedPreferences to settings provider stack
+          sharedPrefsProvider.overrideWithValue(prefs),
+          // Force router to start at /home without auth redirects
+          goRouterProvider.overrideWith((ref) => GoRouter(
+                initialLocation: '/home',
+                routes: [
+                  GoRoute(
+                    path: '/home',
+                    name: 'home',
+                    builder: (context, state) => const HomeScreen(),
+                  ),
+                ],
+              )),
         ],
-        child: const MaterialApp(
-          home: MovieDiscoveryApp(),
-        ),
+        child: const MovieDiscoveryApp(),
       ),
     );
     
